fc8859adecfdb18ace7630fcdd43f7e4c630cfbe
KeiShimon <simonisimonisi@gmail.com>
2019-06-11 12:34:02 +0900

    abc115_C

--- ABC115/ABC115_C_2ndtry.py
+++ ABC115/ABC115_C_2ndtry.py
@@ -25,3 +25,32 @@ for _ in range(N-K+1):
     q.put(nextmin)
 
 print(ans)
+
+
+'''
+Solution2
+'''
+
+import sys
+INPUT = sys.stdin.readline
+
+def SINGLE_INT(): return int(INPUT())
+def MULTIPLE_INT_LIST(): return list(map(int, INPUT().split()))
+def MULTIPLE_INT_MAP(): return map(int, INPUT().split())
+def SINGLE_STRING(): return INPUT()
+def MULTIPLE_STRING(): return INPUT().split()
+
+N, K = MULTIPLE_INT_MAP()
+trees = [int(input()) for _ in range(N)]
+trees.sort()
+
+# faster
+print(min([trees[i+K-1]-trees[i] for i in range(N-K+1)]))
+
+# slower
+ans = float('inf')
+for i in range(0, N-K+1):
+    ans = min(ans, trees[i+K-1] - trees[i])
+    if ans == 0:
+        break
+print(ans)
