5ea3ddecfb23dc70eb065b83016b531cd39185ac
KeiShimon <simonisimonisi@gmail.com>
2019-09-01 22:40:28 +0900

    join ABC139

---
+++ ABC139/A.cpp
@@ -0,0 +1,58 @@
+#include <algorithm>
+#include <cmath>
+#include <iostream>
+#include <numeric>
+#include <string>
+#include <string.h>
+#include <vector>
+
+#define REP(i,x) for(int i{ 0 }; i < (int)(x); i++)
+#define REPC(i,x) for(int i{ 0 }; i <= (int)(x); i++)
+#define RREP(i,x) for(int i{ (int)(x) - 1 }; i >= 0 ;i--)
+#define RREPC(i,x) for(int i{ (int)(x)}; i > 0; i--)
+#define PB push_back
+#define MP make_pair
+#define F first
+#define S second
+#define SZ(x) ((int)(x).size())
+#define ALL(x) (x).begin(),(x).end()
+
+using namespace std;
+
+typedef int64_t int64;
+
+const int dx[4] = { 1, 0, -1,  0 };
+const int dy[4] = { 0, 1,  0, -1 };
+
+template<class T>bool chmax(T& a, const T& b) { if (a < b) { a = b; return 1; } return 0; }
+template<class T>bool chmin(T& a, const T& b) { if (b < a) { a = b; return 1; } return 0; }
+
+using namespace std;
+
+
+class A {
+	string s, t;
+public:
+	A()
+	{
+		cin >> s >> t;
+	}
+	void solve()
+	{
+		int ans{ 0 };
+		if (s[0] == t[0]) ++ans;
+		if (s[1] == t[1]) ++ans;
+		if (s[2] == t[2]) ++ans;
+
+		cout << ans << endl;
+	}
+};
+
+//
+//int main()
+//{
+//	A solution;
+//	solution.solve();
+//
+//	return 0;
+//}
