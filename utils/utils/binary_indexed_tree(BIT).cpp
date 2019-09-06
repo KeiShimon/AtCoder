584755a7102963e7a0e8be88cb657aac00c9e648
KeiShimon <simonisimonisi@gmail.com>
2019-09-06 14:06:21 +0900

    add 2-D BIT implementation in utils

--- utils/utils/binary_indexed_tree(BIT).cpp
+++ utils/utils/binary_indexed_tree(BIT).cpp
@@ -24,7 +24,11 @@ private:
 	int m, n;
 
 public:
+	BIT_2D();
+	BIT_2D(int m, int n, bool initialize_one = false);
 
+	void add(int i, int j, int val);
+	int sum(int i, int j);
 };
 
 
@@ -58,9 +62,50 @@ int BIT::sum(int idx)
 	int ret{ 0 };
 	for (int i{ idx }; i > 0; i -= i & (-i))
 		ret += bit[i];
+
 	return ret;
 }
 
 
-////////// END of 1d BIT implementation //////////
+////////// END of 1-d BIT implementation //////////
 
+
+////////// 2-d BIT implementation //////////
+
+BIT_2D::BIT_2D() : BIT_2D(0, 0) {}
+BIT_2D::BIT_2D(int m, int n, bool initialize_one = false)
+{
+	this->m = m;
+	this->n = n;
+
+	if (!initialize_one)
+		bit.resize(m + 1, vector<int>(n + 1, 0));
+	else
+	{
+		bit.resize(m + 1, vector<int>(n + 1));
+		for(int y{1}; y <= m; y++)
+			for (int x{ 1 }; x <= n; x++)
+				bit[y][x] = (y & (-y)) + (x & (-x));
+	}
+}
+
+void BIT_2D::add(int i, int j, int val)
+{
+	for (int y{ i }; y <= m; y += y & (-y))
+		for (int x{ j }; x <= n; x += x & (-x))
+			bit[y][x] += val;
+
+}
+
+int BIT_2D::sum(int i, int j)
+{
+	int ret{ 0 };
+	for (int y{ i }; y > 0; y -= y & (-y))
+		for (int x{ j }; x > 0; x -= x & (-x))
+			ret += bit[y][x];
+
+	return ret;
+}
+
+
+////////// END of 2-d BIT implementation //////////
