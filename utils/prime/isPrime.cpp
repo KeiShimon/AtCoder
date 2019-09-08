82476a061c1a9525ff82e9598827c8ee2c547ff1
KeiShimon <simonisimonisi@gmail.com>
2019-09-09 01:19:36 +0900

    update (simplify) eratosthenes

--- utils/prime/isPrime.cpp
+++ utils/prime/isPrime.cpp
@@ -8,7 +8,7 @@ bool is_prime(const int64_t n)
 	if (n == 2 || n == 3)
 		return true;
 
-	if ((n % 6 != 1) && (n % 6 != 5))
+	if (!(n & 2) || !(n % 3))
 		return false;
 
 	for (int64_t i{ 5 }; i * i <= n; i += 6)
