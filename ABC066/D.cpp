f13a29aca658558b85e22f4256d30188c558b38c
KeiShimon <simonisimonisi@gmail.com>
2019-09-14 00:39:25 +0900

    complete ABC066

--- ABC066/D.cpp
+++ ABC066/D.cpp
@@ -1,16 +1,138 @@
+#include <algorithm>
+#include <cmath>
+#include <deque>
 #include <iostream>
+#include <numeric>
+#include <sstream>
+#include <string>
+#include <string.h>
+#include <tuple>
+#include <vector>
+#include <unordered_map>
+
+#define REP(i,x) for(int i{ 0 }; i < (int)(x); i++)
+#define REPC(i,x) for(int i{ 0 }; i <= (int)(x); i++)
+#define RREP(i,x) for(int i{ (int)(x) - 1 }; i >= 0 ;i--)
+#define RREPC(i,x) for(int i{ (int)(x)}; i >= 0; i--)
+#define REP1O(i,x) for(int i{ 1 }; i < (int)(x); i++)
+#define REP1C(i,x) for(int i{ 1 }; i <= (int)(x); i++)
+
+#define PB push_back
+#define MP make_pair
+#define F first
+#define S second
+#define SZ(x) ((int)(x).size())
+#define ALL(x) (x).begin(),(x).end()
 
 using namespace std;
 
+typedef int64_t ll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef vector<vector<int>> vvi;
+typedef vector<vector<ll>> vvll;
+typedef pair<int, int> pii;
+typedef tuple<int, int, int> tupiii;
+typedef tuple<ll, ll, ll> tuplll;
+
+const int dx[4] = { 1, 0, -1,  0 };
+const int dy[4] = { 0, 1,  0, -1 };
+const int INTMAX = 2147483647;
+const ll LLMAX = 9223372036854775807;
+
+template<class T>bool chmax(T& a, const T& b) { if (a < b) { a = b; return 1; } return 0; }
+template<class T>bool chmin(T& a, const T& b) { if (b < a) { a = b; return 1; } return 0; }
+void SWAP(ll& a, ll& b) { a ^= b; b ^= a; a ^= b; }
+void SWAP(int& a, int& b) { a ^= b; b ^= a; a ^= b; }
+
+
+const int MOD{ 1000000007 };
+vector<int64_t> factMemo{ 1,1 }, factInvMemo{ 1, 1 }, invMemo{ 0, 1 };
+int curMax{ 1 };
+
+
+void initTable(int n) {
+
+	factMemo.resize(n + 1);
+	factInvMemo.resize(n + 1);
+	invMemo.resize(n + 1);
+
+	for (; curMax < n;)
+	{
+		curMax++;
+		factMemo[curMax] = factMemo[curMax - 1] * curMax % MOD;
+		invMemo[curMax] = MOD - invMemo[MOD % curMax] * (MOD / curMax) % MOD;
+		factInvMemo[curMax] = factInvMemo[curMax - 1] * invMemo[curMax] % MOD;
+	}
+}
+
+
+// calculate mod of combination
+int64_t comb(int n, int k) {
+	if (n < k || n < 0 || k < 0)
+		return 0;
+
+	if (curMax < n)
+		initTable(n);
+
+	return factMemo[n] * (factInvMemo[k] * factInvMemo[n - k] % MOD) % MOD;
+}
+
+
+int64_t comb(int64_t n, int64_t k)
+{
+	return comb((int)n, (int)k);
+}
+
+
 class D {
+	int n;
+	unordered_map<int ,int> um;
+	int left, right;
 
 public:
+	D()
+	{
+		cin >> n;
+		REP(i, n + 1)
+		{
+			int tmp;
+			cin >> tmp;
+
+			auto it = um.find(tmp);
+			if (it != um.end())
+			{
+				left = it->second;
+				right = i;
+				break;
+			}
+			else
+			{
+				um.insert(MP(tmp, i));
+			}
+		}
+	}
+
 	void solve()
 	{
 
+		REP1C(i, n + 1)
+		{
+			if (i == 1)
+				cout << n << endl;
+
+			else {
+				ll ans = comb(n + 1, i) - comb(left + n - right, i - 1);
+
+				if (ans < 0)
+					ans += MOD;
+
+				cout << ans << endl;
+			}
+		}
+
 	}
 };
-
 
 int main()
 {
@@ -19,3 +141,5 @@ int main()
 
 	return 0;
 }
+
+
