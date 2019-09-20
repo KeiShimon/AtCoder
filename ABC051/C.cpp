5581caf5dd298be1122fe662053a9ed0aefeebc6
KeiShimon <simonisimonisi@gmail.com>
2019-09-20 19:59:17 +0900

    complete ABC051

--- ABC051/C.cpp
+++ ABC051/C.cpp
@@ -1,21 +1,85 @@
+#include <algorithm>
+#include <cmath>
+#include <deque>
 #include <iostream>
+#include <numeric>
+#include <queue>
+#include <sstream>
+#include <string>
+#include <string.h>
+#include <tuple>
+#include <vector>
+
+#define REP(i,x) for(int i{ 0 }; i < (int)(x); i++)
+#define REPC(i,x) for(int i{ 0 }; i <= (int)(x); i++)
+#define RREP(i,x) for(int i{ (int)(x) - 1 }; i >= 0 ;i--)
+#define RREPC(i,x) for(int i{ (int)(x)}; i >= 0; i--)
+#define REP1O(i,x) for(int i{ 1 }; i < (int)(x); i++)
+#define REP1C(i,x) for(int i{ 1 }; i <= (int)(x); i++)
+
+#define PB push_back
+#define MP make_pair
+#define F first
+#define S second
+#define SZ(x) ((int)(x).size())
+#define ALL(x) (x).begin(),(x).end()
 
 using namespace std;
 
-class C {
+typedef int64_t ll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef vector<vector<int>> vvi;
+typedef vector<vector<ll>> vvll;
+typedef pair<int, int> pii;
+typedef tuple<int, int, int> tupiii;
+typedef tuple<ll, ll, ll> tuplll;
 
-public:
-	void solve()
-	{
+const int INTMAX = 2147483647;
+const ll LLMAX = 9223372036854775807;
+const int MOD = 1000000007;
 
-	}
-};
+template<class T> inline bool chmax(T& a, const T& b) { if (a < b) { a = b; return 1; } return 0; }
+template<class T> inline bool chmin(T& a, const T& b) { if (b < a) { a = b; return 1; } return 0; }
+inline void swap(ll& a, ll& b) { a ^= b; b ^= a; a ^= b; }
+inline void swap(int& a, int& b) { a ^= b; b ^= a; a ^= b; }
 
+int sx, sy, x, y;
+
+void init()
+{
+	cin >> sx >> sy >> x >> y;
+}
 
 int main()
 {
-	C solution;
-	solution.solve();
+	init();
 
-	return 0;
+	ostringstream oss;
+
+	REP(_i, y - sy)
+		oss << 'U';
+	REP(_i, x - sx)
+		oss << 'R';
+	REP(_i, y - sy)
+		oss << 'D';
+	REP(_i, x - sx)
+		oss << 'L';
+
+	oss << 'L';
+	REP(_i, y - sy + 1)
+		oss << 'U';
+	REP(_i, x - sx + 1)
+		oss << 'R';
+	oss << 'D';
+
+	oss << 'R';
+	REP(_i, y - sy + 1)
+		oss << 'D';
+	REP(_i, x - sx + 1)
+		oss << 'L';
+	oss << 'U';
+
+
+	cout << oss.str() << endl;
 }
