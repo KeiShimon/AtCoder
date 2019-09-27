f70da3aa3341f0d1af9021f960838a7f3998be4f
KeiShimon <simonisimonisi@gmail.com>
2019-09-27 14:29:34 +0900

    complete ABC075

--- ABC075/D.cpp
+++ ABC075/D.cpp
@@ -2,6 +2,7 @@
 #include <cmath>
 #include <deque>
 #include <iostream>
+#include <map>
 #include <numeric>
 #include <queue>
 #include <sstream>
@@ -19,170 +20,95 @@
 
 #define PB push_back
 #define MP make_pair
-#define F first
-#define S second
 #define SZ(x) ((int)(x).size())
 #define ALL(x) (x).begin(),(x).end()
 
 using namespace std;
 
 typedef int64_t ll;
-typedef vector<int> vi;
-typedef vector<ll> vll;
-typedef vector<vector<int>> vvi;
-typedef vector<vector<ll>> vvll;
+typedef vector<int> Vi;
+typedef vector<ll> Vl;
+typedef vector<char> Vc;
+typedef vector<string> Vs;
+typedef vector<bool> Vb;
+typedef vector<vector<int>> VVi;
+typedef vector<vector<ll>> VVl;
 typedef pair<int, int> pii;
-typedef tuple<int, int, int> tupiii;
-typedef tuple<ll, ll, ll> tuplll;
-
-const int INTMAX = 2147483647;
-const ll LLMAX = 9223372036854775807;
-const int MOD = 1000000007;
+typedef pair<ll, ll> pll;
+typedef pair<ll, int> P;
 
 template<class T> inline bool chmax(T& a, const T& b) { if (a < b) { a = b; return 1; } return 0; }
 template<class T> inline bool chmin(T& a, const T& b) { if (b < a) { a = b; return 1; } return 0; }
-void inline swap(ll& a, ll& b) { a ^= b; b ^= a; a ^= b; }
-void inline swap(int& a, int& b) { a ^= b; b ^= a; a ^= b; }
+inline void swap(ll& a, ll& b) { a ^= b; b ^= a; a ^= b; }
+inline void swap(int& a, int& b) { a ^= b; b ^= a; a ^= b; }
+inline void YES() { cout << "YES" << endl; }
+inline void Yes() { cout << "Yes" << endl; }
+inline void NO() { cout << "NO" << endl; }
+inline void No() { cout << "No" << endl; }
 
+const int inf = 1 << 30;
+const ll linf = 1LL << 60;
+const int MOD = 1000000007;
 
-void print(vvi v)
-{
-	REP(i, SZ(v))
-	{
-		REP(j, SZ(v[i]))
-		{
-			cout << v[i][j] << " ";
-		}
-		cout << endl;
-	}
-}
+#include <limits.h>
 
-class D {
-	int n, k;
-	vvll loc;
-	vvi memo;
-
+template <class T>
+struct CumSum2D {
+private:
+	const int h, w;
+	vector<vector<T>> data;
 public:
-	D()
-	{
-		cin >> n >> k;
-		loc.resize(n, vll(2));
-		memo.resize(n, vi(8, 0));
+	CumSum2D(int h, int w) : data(h + 1, vector<T>(w + 1, (T)0)), h{ h + 1 }, w{ w + 1 } {}
+	void add(int i, int j, T val) { if ((i + 1) < h && (j + 1) < w) data[i + 1][j + 1] += val; }
+	void cumulate() { REP1O(i, h) REP1O(j, w) data[i][j] += data[i][j - 1] + data[i - 1][j] - data[i - 1][j - 1]; }
 
-		REP(i, n)
-		{
-			cin >> loc[i][0];
-			cin >> loc[i][1];
-		}
-	}
-
-	void solve()
-	{
-
-		REP(i, n)
-			REP(j, n)
-		{
-			if (i == j)
-				continue;
-
-			if (loc[j][0] > loc[i][0])
-			{
-				++memo[i][0];
-				if (loc[j][1] > loc[i][1])
-				{
-					++memo[i][6];
-					++memo[i][7];
-				}
-				else
-				{
-					++memo[i][2];
-					++memo[i][1];
-				}
-			}
-
-			else
-			{
-				++memo[i][4];
-				if (loc[j][1] > loc[i][1])
-				{
-					++memo[i][6];
-					++memo[i][5];
-				}
-				else
-				{
-					++memo[i][2];
-					++memo[i][3];
-				}
-			}
-		}
-
-		ll ans{ LLMAX };
-
-		REP(i, n)
-			REP(j, n)
-		{
-			if (i == j)
-				continue;
-
-			int cnt = n;
-
-			if (loc[j][0] > loc[i][0])
-			{
-				cnt -= memo[j][0];
-				cnt -= memo[i][4];
-				if (loc[j][1] > loc[i][1])
-				{
-					cnt -= memo[j][6];
-					cnt -= memo[i][2];
-					cnt += memo[i][3];
-					cnt += memo[j][7];
-				}
-				else
-				{
-					cnt -= memo[i][6];
-					cnt -= memo[j][2];
-					cnt += memo[j][1];
-					cnt += memo[i][5];
-				}
-			}
-
-			else
-			{
-				cnt -= memo[i][0];
-				cnt -= memo[j][4];
-				if (loc[j][1] > loc[i][1])
-				{
-					cnt -= memo[j][6];
-					cnt -= memo[i][2];
-					cnt += memo[i][1];
-					cnt += memo[j][5];
-				}
-				else
-				{
-					cnt -= memo[i][6];
-					cnt -= memo[j][2];
-					cnt += memo[j][3];
-					cnt += memo[i][7];
-				}
-			}
-
-			if (k <= cnt)
-			{
-				ll h = abs(loc[i][1] - loc[j][1]);
-				ll w = abs(loc[i][0] - loc[j][0]);
-				chmin(ans, h * w);
-			}
-		}
-
-		cout << ans << endl;
-
-	}
+	// [s, g) , s : btm-left, g : top-right
+	T get(int si, int sj, int gi, int gj) { return data[gi][gj] + data[si][sj] - data[gi][sj] - data[si][gj]; }
 };
+
+int n, k;
+Vi vx, vy;
+VVi pts;
+
+void init()
+{
+	cin >> n >> k;
+	vx.resize(n); vy.resize(n);
+	pts.resize(n, Vi(2));
+	REP(i, n) 
+	{
+		cin >> vx[i] >> vy[i];
+		pts[i][0] = vx[i];
+		pts[i][1] = vy[i];
+	}
+
+	sort(ALL(vx)); sort(ALL(vy));
+}
 
 int main()
 {
-	D solution;
-	solution.solve();
+	init();
 
-	return 0;
+	ll ans = LLONG_MAX;
+
+	CumSum2D<int> cum(n, n);
+	REP(i, n)
+	{
+		int i_x = lower_bound(ALL(vx), pts[i][0]) - vx.begin();
+		int i_y = lower_bound(ALL(vy), pts[i][1]) - vy.begin();
+		cum.add(i_x, i_y, 1);
+	}
+	cum.cumulate();
+
+	REP(l, n - 1)
+		for (int r = l + 1; r < n; ++r)
+		REP(d, n - 1)
+			for (int u = d + 1; u < n; ++u)
+			{
+				int n = cum.get(l, d, r + 1, u + 1);
+				if (n >= k)
+					chmin(ans, (ll)(vx[r] - vx[l]) * (ll)(vy[u] - vy[d]));
+			}
+
+	cout << ans << endl;
 }
