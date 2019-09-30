c28df1df6f9a45230c0c468973f1c01f01aa0cd0
KeiShimon <simonisimonisi@gmail.com>
2019-09-30 16:20:39 +0900

    complete ABC140

--- ABC140/E/E.cpp
+++ ABC140/E/E.cpp
@@ -1,62 +1,28 @@
-#include <algorithm>
-#include <cmath>
-#include <deque>
-#include <iomanip>
 #include <iostream>
-#include <limits.h>
-#include <map>
-#include <numeric>
-#include <queue>
-#include <sstream>
-#include <string>
-#include <string.h>
-#include <tuple>
+#include <set>
 #include <vector>
 
 #define REP(i,x) for(int i{ 0 }; i < (int)(x); i++)
-#define REPC(i,x) for(int i{ 0 }; i <= (int)(x); i++)
-#define RREP(i,x) for(int i{ (int)(x) - 1 }; i >= 0 ;i--)
-#define RREPC(i,x) for(int i{ (int)(x)}; i >= 0; i--)
-#define REP1O(i,x) for(int i{ 1 }; i < (int)(x); i++)
-#define REP1C(i,x) for(int i{ 1 }; i <= (int)(x); i++)
-#define REPIT(i,x) for(auto i{(x).begin()}; i != (x).end(); i++) 
-
-#define PB push_back
-#define MP make_pair
-#define SZ(x) ((int)(x).size())
-#define ALL(x) (x).begin(),(x).end()
 
 using namespace std;
 
 typedef int64_t ll;
-typedef double dbl;
-typedef vector<bool> Vb;
-typedef vector<char> Vc;
-typedef vector<double> Vd;
 typedef vector<int> Vi;
-typedef vector<ll> Vl;
-typedef vector<string> Vs;
-typedef vector<vector<int>> VVi;
-typedef vector<vector<ll>> VVl;
-typedef pair<int, int> pii;
-typedef pair<ll, ll> pll;
-typedef pair<ll, int> pli;
 
-template<class T> inline bool chmax(T& a, const T& b) { if (a < b) { a = b; return 1; } return 0; }
-template<class T> inline bool chmin(T& a, const T& b) { if (b < a) { a = b; return 1; } return 0; }
-inline void swap(ll& a, ll& b) { a ^= b; b ^= a; a ^= b; }
-inline void swap(int& a, int& b) { a ^= b; b ^= a; a ^= b; }
-inline void YES() { cout << "YES" << endl; } inline void Yes() { cout << "Yes" << endl; }
-inline void NO() { cout << "NO" << endl; } inline void No() { cout << "No" << endl; }
-
-const int inf = 1 << 30;
-const ll linf = 1LL << 60;
-const int MOD = 1000000007;
-
+int n;
+Vi vi;
 
 void init()
 {
+	cin >> n;
+	vi.resize(n + 1);
 
+	REP(i, n)
+	{
+		int x;
+		cin >> x;
+		vi[x] = i + 1;
+	}
 }
 
 
@@ -64,5 +30,25 @@ int main()
 {
 	init();
 
+	ll ans = 0;
+	multiset<int> s;
 
+	s.insert(0); s.insert(0);
+	s.insert(n + 1); s.insert(n + 1);
+
+	for (ll x = n; x >= 1; --x)
+	{
+		auto it = s.lower_bound(vi[x]);
+		int i_r = *it++;
+		int i_rr = *it--;
+		int i_l = *(--it);
+		int i_ll = *(--it);
+
+		ans += x * (vi[x] - i_l) * (i_rr - i_r);
+		ans += x * (i_r - vi[x]) * (i_l - i_ll);
+
+		s.insert(vi[x]);
+	}
+
+	cout << ans << endl;
 }
