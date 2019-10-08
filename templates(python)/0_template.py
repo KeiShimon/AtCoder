31d580bd5adc1ba122a258297b1ecd56733ff309
KeiShimon <simonisimonisi@gmail.com>
2019-10-08 19:58:46 +0900

    directory cleanup

--- templates(python)/0_template.py
+++
@@ -1,8 +0,0 @@
-import sys
-INPUT = sys.stdin.readline
-
-def SINGLE_INT(): return int(INPUT())
-def MULTIPLE_INT_LIST(): return list(map(int, INPUT().split()))
-def MULTIPLE_INT_MAP(): return map(int, INPUT().split())
-def SINGLE_STRING(): return INPUT()
-def MULTIPLE_STRING(): return INPUT().split()
