cd613fe472560eb187de4f640485dc4e084486b4
KeiShimon <simonisimonisi@gmail.com>
2019-10-12 22:12:20 +0900

    doing TDPC

--- Typical DP Contest (TDPC)/B/B.cpp
+++ Typical DP Contest (TDPC)/B/B.cpp
@@ -22,28 +22,55 @@
 #define REP1C(i,x) for(int i{ 1 }; i <= (int)(x); i++)
 #define REPIT(i,x) for(auto i{(x).begin()}; i != (x).end(); i++) 
 
-#define PB push_back
-#define MP make_pair
-#define SZ(x) ((int)(x).size())
-#define ALL(x) (x).begin(),(x).end()
-
 using namespace std;
 
-typedef int64_t ll;
-typedef double dbl;
-typedef vector<bool> Vb;
-typedef vector<char> Vc;
-typedef vector<double> Vd;
 typedef vector<int> Vi;
-typedef vector<ll> Vl;
-typedef vector<string> Vs;
-typedef vector<vector<int>> VVi;
-typedef vector<vector<ll>> VVl;
-typedef pair<int, int> pii;
-typedef pair<ll, ll> pll;
-typedef pair<ll, int> pli;
+
+template<class T> inline bool chmax(T& a, const T& b) { if (a < b) { a = b; return 1; } return 0; }
+template<class T> inline bool chmin(T& a, const T& b) { if (b < a) { a = b; return 1; } return 0; }
+
+int n_a, n_b;
+const int max_n = 1000, inf = 1 << 28;
+int va[max_n], vb[max_n];
+int dp[max_n + 2][max_n + 2];
 
 int main()
 {
+	cin >> n_a >> n_b;
+	REP(i, n_a) cin >> va[i];
+	REP(i, n_b) cin >> vb[i];
 
+	RREPC(i, n_a)
+		RREPC(j, n_b)
+	{
+		if (i == n_a && j == n_b)
+			continue;
+
+		if (!((i + j) & 1))
+		{
+			if (i == n_a)
+				dp[i][j] = dp[i][j + 1] + vb[j];
+			else if (j == n_b)
+				dp[i][j] = dp[i + 1][j] + va[i];
+			else
+			{
+				dp[i][j] = dp[i + 1][j] + va[i];
+				chmax(dp[i][j], dp[i][j + 1] + vb[j]);
+			}
+		}
+		else
+		{
+			if (i == n_a)
+				dp[i][j] = dp[i][j + 1];
+			else if (j == n_b)
+				dp[i][j] = dp[i + 1][j];
+			else
+			{
+				dp[i][j] = dp[i + 1][j];
+				chmin(dp[i][j], dp[i][j + 1]);
+			}
+		}
+	}
+
+	cout << dp[0][0] << endl;
 }
